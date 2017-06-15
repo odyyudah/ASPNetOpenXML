64b7f43d2d6298beecb054c22e48df05d1b7a877
odyyudah <odyyudah@gmail.com>
2017-06-15 20:58:48 +0700

    initial commit

---
+++ McDermott.Lib.Office/ExcelReport.cs
@@ -0,0 +1,318 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+//Reference http://mikesknowledgebase.com/pages/CSharp/ExportToExcel.htm 
+
+namespace McDermott.Lib.Office
+{
+    public class ExcelReport
+    {
+        private string _title;
+        private DateTime _printedDate;
+        private string _comments;
+        private string _fileName;
+        private DataSet _dataSet;
+
+        public ExcelReport(DataSet dataSet, String fileName, String title, DateTime printedDate, String comments)
+        {
+            _dataSet = dataSet;
+            _fileName = fileName;
+            _title = title;
+            _printedDate = printedDate;
+            _comments = comments;
+        }
+        public byte[] GenerateExcelReport()
+        {
+            MemoryStream ms = new MemoryStream();
+
+            using (SpreadsheetDocument document = SpreadsheetDocument.Create(ms, SpreadsheetDocumentType.Workbook, true))
+            {
+                WriteExcelFile(document);
+            }
+            ms.Flush();
+            ms.Position = 0;
+            
+            return ms.Length == 0 || ms == null ? null: ms.ToArray();
+        }
+
+        private void WriteExcelFile(SpreadsheetDocument spreadsheet)
+        {
+            //  Create the Excel file contents.  This function is used when creating an Excel file either writing 
+            //  to a file, or writing to a MemoryStream.
+            spreadsheet.AddWorkbookPart();
+            spreadsheet.WorkbookPart.Workbook = new DocumentFormat.OpenXml.Spreadsheet.Workbook();
+
+            //  My thanks to James Miera for the following line of code (which prevents crashes in Excel 2010)
+            spreadsheet.WorkbookPart.Workbook.Append(new BookViews(new WorkbookView()));
+
+            //  If we don't add a "WorkbookStylesPart", OLEDB will refuse to connect to this .xlsx file !
+            WorkbookStylesPart workbookStylesPart = spreadsheet.WorkbookPart.AddNewPart<WorkbookStylesPart>("rIdStyles");
+            Stylesheet stylesheet = GenerateStylesheet();
+            workbookStylesPart.Stylesheet = stylesheet;
+            workbookStylesPart.Stylesheet.Save();
+
+            //  Loop through each of the DataTables in our DataSet, and create a new Excel Worksheet for each.
+            uint worksheetNumber = 1;
+            Sheets sheets = spreadsheet.WorkbookPart.Workbook.AppendChild<Sheets>(new Sheets());
+            foreach (DataTable dt in _dataSet.Tables)
+            {
+                //  For each worksheet you want to create
+                string worksheetName = dt.TableName;
+
+                //  Create worksheet part, and add it to the sheets collection in workbook
+                WorksheetPart newWorksheetPart = spreadsheet.WorkbookPart.AddNewPart<WorksheetPart>();
+                Sheet sheet = new Sheet() { Id = spreadsheet.WorkbookPart.GetIdOfPart(newWorksheetPart), SheetId = worksheetNumber, Name = worksheetName };
+
+                // If you want to define the Column Widths for a Worksheet, you need to do this *before* appending the SheetData
+                // http://social.msdn.microsoft.com/Forums/en-US/oxmlsdk/thread/1d93eca8-2949-4d12-8dd9-15cc24128b10/
+
+                sheets.Append(sheet);
+
+                //  Append this worksheet's data to our Workbook, using OpenXmlWriter, to prevent memory problems
+                WriteDataTableToExcelWorksheet(dt, newWorksheetPart);
+
+                worksheetNumber++;
+            }
+
+            spreadsheet.WorkbookPart.Workbook.Save();
+        }
+
+        private void WriteDataTableToExcelWorksheet(DataTable dt, WorksheetPart worksheetPart)
+        {
+            OpenXmlWriter writer = OpenXmlWriter.Create(worksheetPart, Encoding.ASCII);
+            writer.WriteStartElement(new Worksheet());
+            writer.WriteStartElement(new SheetData());
+
+            string cellValue = "";
+
+            //  Create a Header Row in our Excel file, containing one header for each Column of data in our DataTable.
+            //
+            //  We'll also create an array, showing which type each column of data is (Text or Numeric), so when we come to write the actual
+            //  cells of data, we'll know if to write Text values or Numeric cell values.
+            int numberOfColumns = dt.Columns.Count;
+            bool[] IsNumericColumn = new bool[numberOfColumns];
+            bool[] IsDateColumn = new bool[numberOfColumns];
+
+            string[] excelColumnNames = new string[numberOfColumns];
+            for (int n = 0; n < numberOfColumns; n++)
+                excelColumnNames[n] = GetExcelColumnName(n);
+
+            
+
+            
+            uint rowIndex = 1;
+
+            writer.WriteStartElement(new Row { RowIndex = rowIndex });
+            // Create additional rows above header row
+            //AppendTextCell("1", "report title: "+_title, ref writer, 1);
+            //++rowIndex;
+            //writer.WriteEndElement();
+            //writer.WriteStartElement(new Row { RowIndex = rowIndex });
+            //AppendTextCell("1", "printed at: "+_printedDate, ref writer, 1);
+            //++rowIndex;
+            //writer.WriteEndElement();
+            //writer.WriteStartElement(new Row { RowIndex = rowIndex });
+            //AppendTextCell("1", "comments: "+_comments, ref writer, 1);
+            //++rowIndex;
+            //writer.WriteEndElement();
+            //
+            //  Create the Header row in our Excel Worksheet
+            //
+            for (int colInx = 0; colInx < numberOfColumns; colInx++)
+            {
+                DataColumn col = dt.Columns[colInx];
+                AppendTextCell(excelColumnNames[colInx] + "1", col.ColumnName, ref writer, 2);
+                IsNumericColumn[colInx] = (col.DataType.FullName == "System.Money") || (col.DataType.FullName == "System.Decimal") || 
+                    (col.DataType.FullName == "System.Int32") || (col.DataType.FullName == "System.Double") || (col.DataType.FullName == "System.Single");
+                IsDateColumn[colInx] = (col.DataType.FullName == "System.DateTime") || (col.DataType.FullName == "System.Date");
+            }
+            writer.WriteEndElement();   //  End of header "Row"
+
+            //
+            //  Now, step through each row of data in our DataTable...
+            //
+            double cellNumericValue = 0;
+            foreach (DataRow dr in dt.Rows)
+            {
+                // ...create a new row, and append a set of this row's data to it.
+                ++rowIndex;
+
+                writer.WriteStartElement(new Row { RowIndex = rowIndex });
+
+                for (int colInx = 0; colInx < numberOfColumns; colInx++)
+                {
+                    cellValue = dr.ItemArray[colInx].ToString();
+                    cellValue = ReplaceHexadecimalSymbols(cellValue);
+
+                    // Create cell with data
+                    if (IsNumericColumn[colInx])
+                    {
+                        //  For numeric cells, make sure our input data IS a number, then write it out to the Excel file.
+                        //  If this numeric value is NULL, then don't write anything to the Excel file.
+                        cellNumericValue = 0;
+                        if (double.TryParse(cellValue, out cellNumericValue))
+                        {
+                            cellValue = cellNumericValue.ToString();
+                            AppendNumericCell(excelColumnNames[colInx] + rowIndex.ToString(), cellValue, ref writer, 1);
+                        }
+                    }
+                    else if (IsDateColumn[colInx])
+                    {
+                        //  This is a date value.
+                        DateTime dtValue;
+                        string strValue = "";
+                        if (DateTime.TryParse(cellValue, out dtValue))
+                            strValue = dtValue.ToString("dd-MMM-yyyy");
+                        AppendTextCell(excelColumnNames[colInx] + rowIndex.ToString(), strValue, ref writer, 1);
+                    }
+                    else
+                    {
+                        //  For text cells, just write the input data straight out to the Excel file.
+                        AppendTextCell(excelColumnNames[colInx] + rowIndex.ToString(), cellValue, ref writer, 1);
+                    }
+                }
+                writer.WriteEndElement(); //  End of Row
+            }
+            writer.WriteEndElement(); //  End of SheetData
+            writer.WriteEndElement(); //  End of worksheet
+
+            writer.Close();
+        }
+
+        private void AppendTextCell(string cellReference, string cellStringValue, ref OpenXmlWriter writer, uint styleIndex = 0)
+        {
+            //  Add a new Excel Cell to our Row 
+            writer.WriteElement(new Cell
+            {
+                CellValue = new CellValue(cellStringValue),
+                CellReference = cellReference,
+                DataType = CellValues.String,
+                StyleIndex = styleIndex
+            });
+        }
+
+        private void AppendNumericCell(string cellReference, string cellStringValue, ref OpenXmlWriter writer, uint styleIndex = 0)
+        {
+            //  Add a new Excel Cell to our Row 
+            writer.WriteElement(new Cell
+            {
+                CellValue = new CellValue(cellStringValue),
+                CellReference = cellReference,
+                DataType = CellValues.Number,
+                StyleIndex = styleIndex
+            });
+        }
+
+        private string ReplaceHexadecimalSymbols(string txt)
+        {
+            string r = "[\x00-\x08\x0B\x0C\x0E-\x1F\x26]";
+            return Regex.Replace(txt, r, "", RegexOptions.Compiled);
+        }
+
+        //  Convert a zero-based column index into an Excel column reference  (A, B, C.. Y, Y, AA, AB, AC... AY, AZ, B1, B2..)
+        public string GetExcelColumnName(int columnIndex)
+        {
+            //  eg  (0) should return "A"
+            //      (1) should return "B"
+            //      (25) should return "Z"
+            //      (26) should return "AA"
+            //      (27) should return "AB"
+            //      ..etc..
+            char firstChar;
+            char secondChar;
+            char thirdChar;
+
+            if (columnIndex < 26)
+            {
+                return ((char)('A' + columnIndex)).ToString();
+            }
+
+            if (columnIndex < 702)
+            {
+                firstChar = (char)('A' + (columnIndex / 26) - 1);
+                secondChar = (char)('A' + (columnIndex % 26));
+
+                return string.Format("{0}{1}", firstChar, secondChar);
+            }
+
+            int firstInt = columnIndex / 26 / 26;
+            int secondInt = (columnIndex - firstInt * 26 * 26) / 26;
+            if (secondInt == 0)
+            {
+                secondInt = 26;
+                firstInt = firstInt - 1;
+            }
+            int thirdInt = (columnIndex - firstInt * 26 * 26 - secondInt * 26);
+
+            firstChar = (char)('A' + firstInt - 1);
+            secondChar = (char)('A' + secondInt - 1);
+            thirdChar = (char)('A' + thirdInt);
+
+            return string.Format("{0}{1}{2}", firstChar, secondChar, thirdChar);
+        }
+
+        private Cell ConstructCell(string value, CellValues dataType, uint styleIndex = 0)
+        {
+            return new Cell()
+            {
+                CellValue = new CellValue(value),
+                DataType = new EnumValue<CellValues>(dataType),
+                StyleIndex = styleIndex
+            };
+        }
+
+        private Stylesheet GenerateStylesheet()
+        {
+            Stylesheet styleSheet = null;
+
+            Fonts fonts = new Fonts(
+                new Font( // Index 0 - default
+                    new FontSize() { Val = 10 }
+
+                ),
+                new Font( // Index 1 - header
+                    new FontSize() { Val = 12 },
+                    new Bold(),
+                    new Color() { Rgb = "FFFFFF" }
+
+                ));
+
+            Fills fills = new Fills(
+                    new Fill(new PatternFill() { PatternType = PatternValues.None }), // Index 0 - default
+                    new Fill(new PatternFill() { PatternType = PatternValues.Gray125 }), // Index 1 - default
+                    new Fill(new PatternFill(new ForegroundColor { Rgb = new HexBinaryValue() { Value = "66666666" } })
+                    { PatternType = PatternValues.Solid }) // Index 2 - header
+                );
+
+            Borders borders = new Borders(
+                    new Border(), // index 0 default
+                    new Border( // index 1 black border
+                        new LeftBorder(new Color() { Auto = true }) { Style = BorderStyleValues.Thin },
+                        new RightBorder(new Color() { Auto = true }) { Style = BorderStyleValues.Thin },
+                        new TopBorder(new Color() { Auto = true }) { Style = BorderStyleValues.Thin },
+                        new BottomBorder(new Color() { Auto = true }) { Style = BorderStyleValues.Thin },
+                        new DiagonalBorder())
+                );
+
+            CellFormats cellFormats = new CellFormats(
+                    new CellFormat(), // default
+                    new CellFormat { FontId = 0, FillId = 0, BorderId = 1, ApplyBorder = true }, // body
+                    new CellFormat { FontId = 1, FillId = 2, BorderId = 1, ApplyFill = true } // header
+                );
+
+            styleSheet = new Stylesheet(fonts, fills, borders, cellFormats);
+
+            return styleSheet;
+        }
+    }
+
+}
---
+++ WebOpenXML/Default.aspx.cs
@@ -0,0 +1,107 @@
+﻿using McDermott.Lib.Office;
+using System;
+using System.Data;
+
+namespace WebOpenXML
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+     
+        private DataSet CreateSampleData()
+        {
+            //  Create a sample DataSet, containing three DataTables.
+            //  (Later, this will save to Excel as three Excel worksheets.)
+            //
+            DataSet ds = new DataSet();
+
+            //  Create the first table of sample data
+            DataTable dt1 = new DataTable("Drivers");
+            dt1.Columns.Add("UserID", Type.GetType("System.Decimal"));
+            dt1.Columns.Add("Surname", Type.GetType("System.String"));
+            dt1.Columns.Add("Forename", Type.GetType("System.String"));
+            dt1.Columns.Add("Sex", Type.GetType("System.String"));
+            dt1.Columns.Add("Date of Birth", Type.GetType("System.DateTime"));
+
+            dt1.Rows.Add(new object[] { 1, "James", "Brown", "M", new DateTime(1962, 3, 19) });
+            dt1.Rows.Add(new object[] { 2, "Edward", "Jones", "M", new DateTime(1939, 7, 12) });
+            dt1.Rows.Add(new object[] { 3, "Janet", "Spender", "F", new DateTime(1996, 1, 7) });
+            dt1.Rows.Add(new object[] { 4, "Maria", "Percy", "F", null });
+            dt1.Rows.Add(new object[] { 5, "Malcolm", "Marvelous", "M", new DateTime(1973, 5, 7) });
+            ds.Tables.Add(dt1);
+
+
+            //  Create the second table of sample data
+            DataTable dt2 = new DataTable("Vehicles");
+            dt2.Columns.Add("Vehicle ID", Type.GetType("System.Decimal"));
+            dt2.Columns.Add("Make", Type.GetType("System.String"));
+            dt2.Columns.Add("Model", Type.GetType("System.String"));
+
+            dt2.Rows.Add(new object[] { 1001, "Ford", "Banana" });
+            dt2.Rows.Add(new object[] { 1002, "GM", "Thunderbird" });
+            dt2.Rows.Add(new object[] { 1003, "Porsche", "Rocket" });
+            dt2.Rows.Add(new object[] { 1004, "Toyota", "Gas guzzler" });
+            dt2.Rows.Add(new object[] { 1005, "Fiat", "Spangly" });
+            dt2.Rows.Add(new object[] { 1006, "Peugeot", "Lawnmower" });
+            dt2.Rows.Add(new object[] { 1007, "Jaguar", "Freeloader" });
+            dt2.Rows.Add(new object[] { 1008, "Aston Martin", "Caravanette" });
+            dt2.Rows.Add(new object[] { 1009, "Mercedes-Benz", "Hitchhiker" });
+            dt2.Rows.Add(new object[] { 1010, "Renault", "Sausage" });
+            dt2.Rows.Add(new object[] { 1011, /*char.ConvertFromUtf32(12) + */ "Saab", "Chickennuggetmobile" });
+            ds.Tables.Add(dt2);
+
+
+            //  Create the third table of sample data
+            DataTable dt3 = new DataTable("Vehicle owners");
+            dt3.Columns.Add("User ID", Type.GetType("System.Decimal"));
+            dt3.Columns.Add("Vehicle_ID", Type.GetType("System.Decimal"));
+
+            dt3.Rows.Add(new object[] { 1, 1002 });
+            dt3.Rows.Add(new object[] { 2, 1000 });
+            dt3.Rows.Add(new object[] { 3, 1010 });
+            dt3.Rows.Add(new object[] { 5, 1006 });
+            dt3.Rows.Add(new object[] { 6, 1007 });
+            ds.Tables.Add(dt3);
+
+            return ds;
+        }
+
+        protected void btnExportToExcel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Set initial data
+                string fileName = "Sample.xlsx";
+                string title = "My Report Title";
+                string comments = "This is report comments bla bla bla bla bla bla bla";
+                DataSet ds = CreateSampleData();
+                //Process Dataset
+                ExcelReport myreport = new ExcelReport(ds, fileName, title, DateTime.Now, comments);
+                byte[] data = myreport.GenerateExcelReport();
+                Response.ClearContent();
+                Response.Clear();
+                Response.Buffer = true;
+                Response.Charset = "";
+                
+                Response.Cache.SetCacheability(System.Web.HttpCacheability.NoCache);
+                Response.AddHeader("content-disposition", "attachment; filename=" + fileName);
+                Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                
+                Response.BinaryWrite(data);
+                Response.Flush();
+
+                System.Web.HttpContext.Current.Response.Flush();
+                System.Web.HttpContext.Current.Response.SuppressContent = true;
+                System.Web.HttpContext.Current.ApplicationInstance.CompleteRequest();
+
+            }
+            catch (Exception ex)
+            {
+                string s = ex.Message;
+            }
+        }
+    }
+}
